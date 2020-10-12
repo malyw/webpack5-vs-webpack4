28a3baa80c62c18e9eda80dd59719622a3513515
shospodarets <shospodarets@guidewire.com>
2020-10-12 01:52:40 +0100

    Top level await exercise with Webpack 5

--- webpack5/src/index.js
+++ webpack5/src/index.js
@@ -1,4 +1,7 @@
 import Form from "./js/components/Form";
 
+const translatedStrings = await import(`./i18n/es_ES.json`);
+console.log('translatedStrings', translatedStrings);
+
 const wrapper = document.getElementById("container");
 wrapper ? ReactDOM.render(<Form />, wrapper) : false;
--- webpack5/webpack.config.js
+++ webpack5/webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    experiments: {
+        topLevelAwait: true,
+    },
     module: {
         rules: [
             {
